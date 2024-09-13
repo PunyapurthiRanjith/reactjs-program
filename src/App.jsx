66b660e2bb049186765412f591f142e299de3160
Punyapurthi Ranjith <ranjithpunyapurthi03@gmail.com>
2024-09-13 18:31:19 +0530

    fetching Fake store products in class compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import YoutubeButton from "./components/class/youtube-btn.jsx";
-import DynamicTable from "./components/dynamic-table/table.jsx";
+import FakeStoreCategories from "./components/class/category-product.jsx";
+import FakeStoreProducts from "./components/class/products.jsx";
 
 function App() {
   return (
     <div>
-      <YoutubeButton/>
-      <DynamicTable/>
+      <FakeStoreProducts/>
+      <FakeStoreCategories/>
     </div>
   );
 }
